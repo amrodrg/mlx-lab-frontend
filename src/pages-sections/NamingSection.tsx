646d41a497eb16cbd4ab8670c7be7b0f44d77795
Amr <ad@mvp-lab>
2022-03-04 19:46:05 +0100

    Update NamingSection.tsx

--- src/pages-sections/NamingSection.tsx
+++ src/pages-sections/NamingSection.tsx
@@ -36,7 +36,7 @@ export default function NamingSection() {
             type="button"
             className="h-16 w-16 rounded-full bg-transparent place-self-center"
           >
-            <ArrowNarrowDownIcon className="h-10 w-10 bg-white rounded-full text-main-blue"/>
+            <ArrowNarrowDownIcon className="h-10 w-10 bg-white rounded-full text-main-blue p-1"/>
           </button>
         </div>
       </div>
